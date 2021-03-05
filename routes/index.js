be23cbc9f44b35bee652b0cd479407c4ed1ba6e3
firebfm <firebfm@gmail.com>
2021-03-05 15:23:09 -0500

    add navbar, login and logout routes

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,11 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const router = express.Router();
 const moment = require('moment');
+const e = require('express');
 moment().format();
-
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
-
 
 app.use(cookieSession({
   name: 'session',
@@ -27,7 +26,50 @@ app.use(cookieSession({
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render('index');
+    if (req.session.user_id) {
+      res.redirect('/index');
+    } else {
+      res.redirect('/login');
+    }
+  })
+
+  router.get("/portfolio", (req, res) => {
+    const templateVars = { user: req.session.user };
+    if (req.session.user) {
+      res.render("portfolio", templateVars);
+    } else {
+      res.redirect('/login');
+    }
+  });
+
+  router.get("/login", (req, res) => {
+    const templateVars = { user: req.session.user };
+    res.render("login", templateVars);
+  });
+
+  router.post("/logout", (req, res) => {
+    req.session = null;
+    res.redirect('/');
+  });
+
+  router.post("/login", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    db.query(`
+    SELECT * FROM users
+    WHERE username = $1;
+    ;`, [username])
+      .then(data => {
+        if (data.rows[0].password === password) { 
+          req.session.user = data.rows[0];
+          res.redirect("/portfolio");
+        } else {
+          res.status(403).send('Wrong password');
+        }
+      })
+      .catch(err => {
+        res.status(500).redirect('error', err.message);
+      });
   })
   return router;
 }
